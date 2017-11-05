f339be932037063a29b7b6d0b9c054a6fbf21065
Chev Eldrid <chev424@gmail.com>
2017-11-05 15:32:17 -0500

    Updates
    Attempting to fix some js warnings and editing some content from previous blog entries

--- js/picture-story.js
+++ js/picture-story.js
@@ -1,24 +1,31 @@
+
 function work() {
-    document.getElementById("about-head").innerHTML="GMO";
-    document.getElementById("about-text").innerHTML="For my first six-month co-op I knew I wanted to do something a little different than straight Computer Science. I wanted to work in an industry that applied the methods I was taught but built off of them. So I accepted a position at GMO, an asset management company as part of the Data Governance team. I use mostly SQL and a Data management platform in my day-to-day. What I <i>really</i> want to do is continue adding automation to the Data Governance workflow, so far its all been in-platform but I look forward to learning about API connectivity in creating external scripts to do some of the more menial data science work>";
+    "use strict";
+    document.getElementById("about-head").innerHTML = "GMO";
+    document.getElementById("about-text").innerHTML = "For my first six-month co-op I knew I wanted to do something a little different than straight Computer Science. I wanted to work in an industry that applied the methods I was taught but built off of them. So I accepted a position at GMO, an asset management company as part of the Data Governance team. I use mostly SQL and a Data management platform in my day-to-day. What I <i>really</i> want to do is continue adding automation to the Data Governance workflow, so far its all been in-platform but I look forward to learning about API connectivity in creating external scripts to do some of the more menial data science work>";
 }
 function school() {
-    document.getElementById("about-head").innerHTML="Northeastern University";
-    document.getElementById("about-text").innerHTML="Located in the heart of Boston, Northeastern has given me many of the tools I need to pursue a career in tech. From tutoring Fundamentals of Computer science to building a website from scratch with my fraternity, nothing has seemed impossible.";
+    "use strict";
+    document.getElementById("about-head").innerHTML = "Northeastern University";
+    document.getElementById("about-text").innerHTML = "Located in the heart of Boston, Northeastern has given me many of the tools I need to pursue a career in tech. From tutoring Fundamentals of Computer science to building a website from scratch with my fraternity, nothing has seemed impossible.";
 }
 function china() {
-    document.getElementById("about-head").innerHTML="China";
-    document.getElementById("about-text").innerHTML="For a few years now I've wanted to study Mandarin, learn something so incredibly different from English or any other western language. So last summer when I was given the opportunity to travel to Kunming, China and study for a half semester at Yunnan University I jumped on it! While I started formal Mandarin studies the year before, I was not at all prepared for the demands of total immersion. That being said, apart from once trying to order milk tea from a Dumpling shop... I did reasonably okay! Spending mornings in class and afternoons exploring the city with local language partners it really was an unforgettable experience.";
+    "use strict";    
+    document.getElementById("about-head").innerHTML = "China";
+    document.getElementById("about-text").innerHTML = "For a few years now I've wanted to study Mandarin, learn something so incredibly different from English or any other western language. So last summer when I was given the opportunity to travel to Kunming, China and study for a half semester at Yunnan University I jumped on it! While I started formal Mandarin studies the year before, I was not at all prepared for the demands of total immersion. That being said, apart from once trying to order milk tea from a Dumpling shop... I did reasonably okay! Spending mornings in class and afternoons exploring the city with local language partners it really was an unforgettable experience.";
 }
 function italy() {
-    document.getElementById("about-head").innerHTML="Italy";
-    document.getElementById("about-text").innerHTML="One of the things I love to do most is travel: be it just a road trip with friends or spending a summer studying abroad I try to take every opportunity I can. After Freshman year, I joined a Northeastern program that spent a month exploring Italy. Focusing on science and technological developments after the Roman Empire, we spent a lot of the time learning about the Rennaissance and the many revolutions of the time period. Be it the expansive influence of Da Vinci or the economics of coffee, there was always something new to learn. I really do hope to get back some day.";
+    "use strict";
+    document.getElementById("about-head").innerHTML = "Italy";
+    document.getElementById("about-text").innerHTML = "One of the things I love to do most is travel: be it just a road trip with friends or spending a summer studying abroad I try to take every opportunity I can. After Freshman year, I joined a Northeastern program that spent a month exploring Italy. Focusing on science and technological developments after the Roman Empire, we spent a lot of the time learning about the Rennaissance and the many revolutions of the time period. Be it the expansive influence of Da Vinci or the economics of coffee, there was always something new to learn. I really do hope to get back some day.";
 }
 function home() {
-    document.getElementById("about-head").innerHTML="Saratoga Springs, New York";
-    document.getElementById("about-text").innerHTML="Saratoga is home to a racetrack and the site of a critical Revolutionary War battle. Personally, I'm more interested in the nearby orchards, driving to Taco Bell at 2 A.M. for some Baja Blast, going downtown to play firsbee in the park, or kayaking in the Summer. I think Saratoga is a fantastic town to grow up in, and I still try to visit home whenever I can.";
+    "use strict";
+    document.getElementById("about-head").innerHTML = "Saratoga Springs, New York";
+    document.getElementById("about-text").innerHTML = "Saratoga is home to a racetrack and the site of a critical Revolutionary War battle. Personally, I'm more interested in the nearby orchards, driving to Taco Bell at 2 A.M. for some Baja Blast, going downtown to play firsbee in the park, or kayaking in the Summer. I think Saratoga is a fantastic town to grow up in, and I still try to visit home whenever I can.";
 }
 function hobby() {
-    document.getElementById("about-head").innerHTML="Hobbies";
-    document.getElementById("about-text").innerHTML="When I'm not working or otherwise coding I like to do any number of things: whether it be meeting up with a friend to play some videogames, getting together with high school buddies for some <i>Magic: The Gathering</i>, going on an <a href='http://chevstravels.blogspot.com/'>adventure</a>, or just spending an afternoon reading a book. I feel it's important to find a balance. ";
+    "use strict";
+    document.getElementById("about-head").innerHTML = "Hobbies";
+    document.getElementById("about-text").innerHTML = "When I'm not working or otherwise coding I like to do any number of things: whether it be meeting up with a friend to play some videogames, getting together with high school buddies for some <i><a href='http://tappedout.net/users/Nero64/mtg-decks/'>Magic: The Gathering</a></i>, going on an <a href='http://chevstravels.blogspot.com/'>adventure</a>, or just spending an afternoon reading a book. I feel it's important to find a balance. ";
 }
